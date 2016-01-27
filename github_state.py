1d985f4c32e2e4a0bff54398c2ce6ecdf89aba6c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-01-27 11:39:49 -0500

    Add debug output

--- github_state.py
+++ github_state.py
@@ -4,6 +4,22 @@ from __future__ import print_function, division
 
 import requests
 import pandas as pd
+import logging
+
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 import matplotlib
 if __name__ == '__main__':
     matplotlib.use('svg')
